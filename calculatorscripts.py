cf154e1463b1224006c6b4f6069f35326e2476b1
Eric Petersen <Kire137.p@gmail.com>
2025-01-14 06:57:52 +0000

    more things

---
+++ calculatorscripts.py
@@ -0,0 +1,26 @@
+from numpy import random
+
+a=[1, 2, 3, 4, 5, 6]
+
+def checknum(n):
+    for x in a:
+        if n==x:
+            return(True)
+    else:
+          return(False)
+
+n=int(input("open "))
+c=checknum(n)
+
+
+if c==False:
+    print("redo")
+else:
+    pass
+
+k=random.randint(6)
+
+if n==k:
+    print("you fucking die")
+else:
+    print("got lucky bitch")
